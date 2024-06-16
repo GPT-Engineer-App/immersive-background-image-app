284ff48dbb225a50388c0256585d712b8484e11f
Mark24 <125602043+MarkW1919@users.noreply.github.com>
2024-06-16 15:06:58 -0500

    Sales Manager component addition

---
+++ src/components/SalesManagerDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SalesManagerDashboard = () => {
+  return (
+    <div>
+      <h1>Sales Manager Dashboard</h1>
+      {/* Add your Sales Manager Dashboard content here */}
+    </div>
+  );
+};
+
+export default SalesManagerDashboard;
