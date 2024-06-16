e2fae4bc46cdb09d27daa5bb11e0087f875f1454
Mark24 <125602043+MarkW1919@users.noreply.github.com>
2024-06-16 13:15:25 -0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,22 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Index from "./pages/Index.jsx";
-import About from "./pages/About.jsx";
-import Contact from "./pages/Contact.jsx";
-import Logout from "./pages/Logout.jsx";
-import SalesManagerDashboard from "./pages/SalesManagerDashboard.jsx";
-import SalesmanDashboard from "./pages/SalesmanDashboard.jsx";
-import Leads from "./pages/Leads.jsx";
-import Appointments from "./pages/Appointments.jsx";
-import CsvUpload from "./pages/CsvUpload.jsx";
-import Login from "./pages/Login.jsx";
-import PrivateRoute from "./components/PrivateRoute.jsx";
-import Navbar from "./components/Navbar.jsx";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import AdminDashboard from './components/AdminDashboard';
 
 function App() {
+  // Hardcoded authentication bypass
+  const isAuthenticated = true;
+
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Index />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/contact" element={<Contact />} />
-          <Route exact path="/logout" element={<Logout />} />
-          <Route exact path="/sales-manager" element={<PrivateRoute><SalesManagerDashboard /></PrivateRoute>} />
-          <Route exact path="/salesman" element={<PrivateRoute><SalesmanDashboard /></PrivateRoute>} />
-          <Route exact path="/leads" element={<PrivateRoute><Leads /></PrivateRoute>} />
-          <Route exact path="/appointments" element={<PrivateRoute><Appointments /></PrivateRoute>} />
-          <Route exact path="/csv-upload" element={<PrivateRoute><CsvUpload /></PrivateRoute>} />
-          <Route exact path="/login" element={<Login />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Switch>
+        {isAuthenticated ? (
+          <Route path="/" component={AdminDashboard} />
+        ) : (
+          <Route path="/" component={LoginPage} />
+        )}
+      </Switch>
+    </Router>
   );
 }
 
